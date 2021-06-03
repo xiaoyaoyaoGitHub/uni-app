1eef32b760a530bae6bc8116d7c7dce7bb6b8b09
zhm <zhuhuimin@yinmaisoft.com>
2021-06-03 10:04:07 +0800

    代码优化

--- pages/my/contactUs/index.vue
+++ pages/my/contactUs/index.vue
@@ -4,9 +4,9 @@
 			<image :src="contactus" mode="widthFix"></image>
 		</view>
 		<view class="contactus u-m-t-30 u-flex-col">
-			<view class="u-flex items u-m-b-20" v-for="(item,index) in list" @click="Jump(index)">
+			<view class="u-flex items u-m-b-20" v-for="(item,index) in list" @click="Jump(index)" :key="index">
 				<view :class="item.bcg" style="" class="items-iconBox u-m-r-50 u-padding-15">
-					<u-icon :name="item.icon" color="#ffffff" size="58"></u-icon>
+					<u-icon :name="item.icon" color="#ffffff" size="54"></u-icon>
 				</view>
 				<view class="u-flex-col u-flex-1">
 					<text>{{item.name}}</text>
@@ -16,23 +16,18 @@
 					<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
 				</view>
 			</view>
-
 			<view class="serviceTime u-flex-col u-p-l-32 u-p-b-20 u-p-t-20">
 				<text class="u-font-xl" type='title'>服务时间</text>
 				<text class="textSize"><text style="color: #303133;">工作日：</text>{{workingHours}}</text>
 				<text class="textSize"><text style="color: #303133;">节假日：</text>{{holidayWorkingHours}}</text>
 			</view>
-
 		</view>
-		<view class="copyright u-font-13"><text>Copyright © 2021 引迈信息技术有限公司出品</text></view>
-		<uni-popup type="center" mode="insert" :mask-click="false" ref="popup">
+		<view class="copyright u-font-26"><text>Copyright © 2021 引迈信息技术有限公司出品</text></view>
+		<u-popup v-model="showPopup" mode="center">
 			<view class="center-box">
 				<image class="image" :src="wechat_qrcode" :data-path="wechat_qrcode" @longpress="saveImage" />
-				<view class="u-text-center u-m-t-20" @click="cancel('image')">
-					<u-icon name="close-circle-fill" color="#ffffff" size="60"></u-icon>
-				</view>
 			</view>
-		</uni-popup>
+		</u-popup>
 	</view>
 </template>
 
@@ -46,7 +41,8 @@
 				holidayWorkingHours: '9:30-12:00 ，13:30-17:30',
 				workingHours: '8:30-12:00 ，13:00-20:00',
 				tell: '400-6868-969',
-				url:'https://www.jnpfsoft.com',
+				url: 'https://www.jnpfsoft.com',
+				showPopup: false,
 				list: [{
 						name: '微信公众号',
 						title: '扫码关注官网微信公众号',
@@ -72,7 +68,7 @@
 			Jump(index) {
 				switch (index) {
 					case 0:
-						this.$refs.popup.open()
+						this.showPopup = true
 						break;
 					case 1:
 						uni.makePhoneCall({
@@ -95,6 +91,7 @@
 				}
 			},
 			saveImage(e) {
+				// #ifdef APP-PLUS
 				uni.getImageInfo({
 					src: e.currentTarget.dataset.path,
 					success: (res) => {
@@ -106,9 +103,7 @@
 						});
 					}
 				});
-			},
-			cancel() {
-				this.$refs.popup.close()
+				// #endif
 			},
 		}
 	}
@@ -139,6 +134,11 @@
 
 				.items-iconBox {
 					border-radius: 50%;
+					height: 88rpx;
+					width: 88rpx;
+					display: flex;
+					align-items: center;
+					justify-content: center;
 				}
 			}
 
@@ -149,8 +149,8 @@
 		}
 
 		.center-box {
-			width: 360rpx;
-			height: 360rpx;
+			width: 420rpx;
+			height: 420rpx;
 
 			image {
 				width: 100%;
