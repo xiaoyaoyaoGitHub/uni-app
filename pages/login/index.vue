6b44d931179b10975ddd6321f72d712e60b8a28f
zengjunfeng <20871209@qq.com>
2021-06-04 15:05:16 +0800

    新增交付登录页

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -4,56 +4,35 @@
 			<view class="logoImg">
 				<image src="../../static/logo.png" mode="widthFix"></image>
 			</view>
+			<view class="u-flex-col introduce u-m-t-30">
+				<text class="u-font-36 text-one" style="">JNPF快速开发平台</text>
+				<text class="u-font-24 text-two">低代码，企业数字化好帮手</text>
+			</view>
 			<view class="loginSwitch u-flex-col">
-				<view class="tabs" :class="{'active2': loginMethod == 'password'}">
-					<!-- loginMethod == 'password' ? 'active2' : 'active1' -->
-					<text class="tab u-m-r-64 u-font-28" :class="{'active':loginMethod == 'verification'}"
-						@click="changeTab('verification')">验证码登录</text>
-					<text class="tab u-m-l-64 u-font-28" :class="{'active':loginMethod == 'password'}"
-						@click="changeTab('password')">密码登录</text>
-				</view>
 				<view class="loginInputBox u-flex-col">
 					<u-form :model="formData" :rules="rules" ref="dataForm" :errorType="['toast']" label-position="left"
 						label-width="150" label-align="left">
 						<u-form-item prop="account">
 							<u-input v-model="formData.account" placeholder="请输入帐号"></u-input>
 						</u-form-item>
-
-						<u-form-item prop="password" v-if="loginMethod == 'password'">
+						<u-form-item prop="password">
 							<u-input v-model="formData.password" type="password" placeholder="请输入密码"></u-input>
 						</u-form-item>
-						<u-form-item :label-position="labelPosition" prop="smsCode" label-width="150"
-							v-if="loginMethod == 'verification'">
-							<u-input :border="border" placeholder="请输入验证码" v-model="formData.smsCode" type="text">
-							</u-input>
-							<u-button slot="right" type="primary" size="mini" @click="getCode">{{codeTips}}</u-button>
-						</u-form-item>
-						<view v-if="loginMethod == 'verification' && isFirstLogin">
-							<u-form-item prop="userName">
-								<u-input v-model="formData.userName" type="text" placeholder="请输入姓名"></u-input>
-							</u-form-item>
-							<u-form-item prop="companyName">
-								<u-input v-model="formData.companyName" type="text" placeholder="请输入公司名"></u-input>
-							</u-form-item>
-						</view>
 					</u-form>
 					<view class="loginBtnBox u-m-t-64">
-						<u-button @click="defaultLogin" type="primary" :loading="loading">{{ loading ? "登录中...":"登录"}}
+						<u-button @click="login" type="primary" :loading="loading">{{ loading ? "登录中...":"登录"}}
 						</u-button>
 					</view>
 
 				</view>
 			</view>
 		</view>
-		<u-verification-code seconds="60" ref="uCode" @change="codeChange"></u-verification-code>
 	</view>
 </template>
 
 <script>
 	import {
-		login,
-		clickSms,
-		loginSms
+		login
 	} from '@/api/common.js'
 	import md5Libs from "uview-ui/libs/function/md5";
 	import resources from '@/libs/resources'
@@ -61,21 +40,11 @@
 	export default {
 		data() {
 			return {
-				codeTips: '',
-				border: false,
-				labelPosition: 'left',
 				loading: false,
-				loginMethod: 'verification',
-				mode: 1,
 				formData: {
 					account: "",
 					password: "",
-					userName: '',
-					companyName: '',
-					smsCode: ''
 				},
-				isFirstLogin: false,
-				code: '获取验证码',
 				rules: {
 					account: [{
 						required: true,
@@ -87,27 +56,6 @@
 						message: '请输入密码',
 						trigger: 'blur',
 					}],
-					userName: [{
-						required: true,
-						message: '请输入姓名',
-						trigger: 'blur',
-					}],
-					companyName: [{
-						required: true,
-						message: '请输入公司名',
-						trigger: 'blur',
-					}],
-					smsCode: [{
-							required: true,
-							message: '请输入验证码',
-							trigger: ['change', 'blur'],
-						},
-						{
-							type: 'number',
-							message: '验证码只能为数字',
-							trigger: ['change', 'blur'],
-						}
-					],
 				}
 			}
 		},
@@ -115,90 +63,16 @@
 			this.$refs.dataForm.setRules(this.rules);
 		},
 		onLoad() {
-
+			this.formData.password = ''
 		},
 		methods: {
-			changeTab(loginMethod) {
-				this.formData.password = '';
-				this.formData.userName = '';
-				this.formData.companyName = '';
-				this.formData.smsCode = '';
-				this.isFirstLogin = false;
-				this.loginMethod = loginMethod;
-			},
-			codeChange(text) {
-				this.codeTips = text;
-			},
-			// 获取验证码
-			getCode() {
-				const valid = this.$u.test.mobile(this.formData.account)
-				if (!valid) return this.$u.toast('请输入正确手机号！')
-				if (this.$refs.uCode.canGetCode) {
-					uni.showLoading({
-						title: '正在获取验证码',
-						mask: true
-					})
-					setTimeout(() => {
-						uni.hideLoading();
-						clickSms(this.formData.account).then(res => {
-							this.$u.toast('验证码已发送');
-
-							if (res.data == 0) this.isFirstLogin = true;
-						}).catch(() => {
-
-						})
-						this.$refs.uCode.start();
-					}, 2000);
-				} else {
-					this.$u.toast('倒计时结束后再发送');
-				}
-			},
-
-			/* 点击登录 */
-			defaultLogin() {
-
-				let _this = this
-				if (this.loginMethod == 'verification') {
-					this.loginSms()
-
-				} else {
-
-					this.login(this.formData.account, this.formData.password)
-				}
-			},
-
-			//验证码登录
-			loginSms() {
-				this.$refs.dataForm.validate(valid => {
-					if (valid) {
-						let data = {
-							mode: this.mode, //后端语言
-							mobile: this.formData.account, //电话
-							smsCode: this.formData.smsCode, //短信码
-							contacts: this.formData.userName, //用户名称
-							company: this.formData.companyName //公司名字
-						}
-						loginSms(data).then(res => {
-
-							this.login(this.formData.account, res.data)
-						}).catch(() => {
-
-						})
-					} else {
-
-					}
-				});
-			},
-
-
-			//公共登录
-			login(account, password) {
+			login() {
 				this.$refs.dataForm.validate(valid => {
 					if (valid) {
 						this.loading = true
 						let query = {
-							account: account,
-							password: md5Libs.md5(password)
+							account: this.formData.account,
+							password: md5Libs.md5(this.formData.password)
 						}
 						// #ifdef  APP-PLUS
 						const clientId = plus.push.getClientInfo().clientid;
@@ -241,11 +115,21 @@
 					border-radius: 20%;
 				}
 			}
-
+			.introduce{
+				justify-content: center;
+				align-items: center;
+				.text-one{
+					height: 70rpx;
+					font-weight: 700;
+				}
+				.text-two{
+					color: #999999;
+				}
+			}
 		}
 
 		.loginSwitch {
-			margin-top: 100rpx;
+			margin-top: 40rpx;
 			justify-content: center;
 			align-items: center;
 
