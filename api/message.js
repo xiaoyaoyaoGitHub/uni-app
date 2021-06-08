3699a8e43f63bf3e576dff138a9a73a378597def
zengjunfeng <20871209@qq.com>
2021-06-08 09:51:08 +0800

    新增公告列表，公告详情，系统消息列表

--- api/message.js
+++ api/message.js
@@ -6,3 +6,33 @@ export function getIMReply() {
 		url: '/api/message/imreply',
 	})
 }
+//获取公告列表
+export function getNoticeList(data) {
+	return request({
+		method: 'GET',
+		url: '/api/message',
+		data
+	})
+}
+//公告详情
+export function getNoticeDetail(id) {
+	return request({
+		method: 'GET',
+		url: `/api/message/ReadInfo/${id}`
+	})
+}
+//获取系统消息列表
+export function getSysMsgList(data) {
+	return request({
+		method: 'GET',
+		url: '/api/message',
+		data
+	})
+}
+//系统消息详情
+export function sysMsgInfo(id) {
+	return request({
+		method: 'GET',
+		url: `/api/message/ReadInfo/${id}`
+	})
+}
