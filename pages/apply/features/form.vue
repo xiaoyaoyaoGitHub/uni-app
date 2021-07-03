bace8b6c43c6c3f0a8090606e2322b6fbcb14497
zengjunfeng <20871209@qq.com>
2021-07-03 14:08:34 +0800

    移动自定义表单必填

--- pages/apply/features/form.vue
+++ pages/apply/features/form.vue
@@ -37,63 +37,63 @@
 				</u-form-item>
 
 				<u-form-item :label="items.__config__.label" v-if="items.__config__.jnpfKey == 'comInput'">
-					<u-input placeholder="请输入" v-model="dataForm[items.__vModel__]"></u-input>
+					<u-input placeholder="请输入" v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></u-input>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" v-if="items.__config__.jnpfKey == 'textarea'">
-					<u-input placeholder="请输入" v-model="dataForm[items.__vModel__]" type="textarea"></u-input>
+					<u-input placeholder="请输入" v-model="dataForm[items.__vModel__]" type="textarea" :required='items.__config__.required'></u-input>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="checkbox"
 					v-if="items.__config__.jnpfKey == 'checkbox'">
 					<jnpf-select v-model="dataForm[items.__vModel__]" placeholder="请选择下拉框组"
-						:options="items.__config__.options" :props="items.__config__.props" multiple>
+						:options="items.__config__.options" :props="items.__config__.props" multiple :required='items.__config__.required'>
 					</jnpf-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="numInput"
 					v-if="items.__config__.jnpfKey == 'numInput'">
 					<u-number-box v-model="dataForm[items.__vModel__]" :min="items.min" :max="items.max"
-						:step="items.step" :input-width="120" :positive-integer="false" :input-height="60">
+						:step="items.step" :input-width="120" :positive-integer="false" :input-height="60" :required='items.__config__.required'>
 					</u-number-box>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="select" v-if="items.__config__.jnpfKey == 'select'">
 					<jnpf-select v-model="dataForm[items.__vModel__]" placeholder="请选择下拉框组"
-						:options="items.__config__.options" :props="items.__config__.props">
+						:options="items.__config__.options" :props="items.__config__.props" :required='items.__config__.required'>
 					</jnpf-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="radio" v-if="items.__config__.jnpfKey == 'radio'">
 					<u-radio-group v-model="dataForm[items.__vModel__]">
 						<u-radio v-for="(radios, radioIndex) in items.__config__.options" :key="radioIndex"
-							:name="radios[items.__config__.props.value]">
+							:name="radios[items.__config__.props.value]" :required='items.__config__.required'>
 							{{ radios[items.__config__.props.label] }}
 						</u-radio>
 					</u-radio-group>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="switch" v-if="items.__config__.jnpfKey == 'switch'">
-					<jnpf-switch v-model="dataForm[items.__vModel__]"></jnpf-switch>
+					<jnpf-switch v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-switch>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="cascader"
 					v-if="items.__config__.jnpfKey == 'cascader'">
 					<jnpf-cascader v-model="dataForm[items.__vModel__]" placeholder="请选择级联选择"
-						:options="items.__config__.options">
+						:options="items.__config__.options" :required='items.__config__.required'>
 					</jnpf-cascader>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="time" v-if="items.__config__.jnpfKey == 'time'">
-					<jnpf-date-time type="time" v-model="dataForm[items.__vModel__]"></jnpf-date-time>
+					<jnpf-date-time type="time" v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-date-time>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="date" v-if="items.__config__.jnpfKey == 'date'">
-					<jnpf-date-time type="date" v-model="dataForm[items.__vModel__]"></jnpf-date-time>
+					<jnpf-date-time type="date" v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-date-time>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="uploadImg"
 					v-if="items.__config__.jnpfKey == 'uploadImg'">
-					<jnpf-upload v-model="dataForm[items.__vModel__]"></jnpf-upload>
+					<jnpf-upload v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-upload>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="rate" v-if="items.__config__.jnpfKey == 'rate'">
 					<u-rate v-model="dataForm[items.__vModel__]" size="40"
-						@change="changeRate($event,items.__vModel__)">
+						@change="changeRate($event,items.__vModel__)" :required='items.__config__.required'>
 					</u-rate>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="slider" v-if="items.__config__.jnpfKey == 'slider'">
 					<u-slider v-model="dataForm[items.__vModel__]" :step="items.step" :min="items.min" :max="items.max"
-						style="width: 100%;">
+						style="width: 100%;" :required='items.__config__.required'>
 						<view class="">
 							<view class="badge-button">
 								{{dataForm[items.__vModel__]}}
@@ -103,19 +103,19 @@
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="comSelect"
 					v-if="items.__config__.jnpfKey == 'comSelect'">
-					<jnpf-org-select type="organize" v-model="dataForm[items.__vModel__]"></jnpf-org-select>
+					<jnpf-org-select type="organize" v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-org-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="depSelect"
 					v-if="items.__config__.jnpfKey == 'depSelect'">
-					<jnpf-org-select type="department" v-model="dataForm[items.__vModel__]"></jnpf-org-select>
+					<jnpf-org-select type="department" v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-org-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="userSelect"
 					v-if="items.__config__.jnpfKey == 'userSelect'">
-					<jnpf-org-select v-model="dataForm[items.__vModel__]"></jnpf-org-select>
+					<jnpf-org-select v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-org-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="posSelect"
 					v-if="items.__config__.jnpfKey == 'posSelect'">
-					<jnpf-org-select type="position" v-model="dataForm[items.__vModel__]"></jnpf-org-select>
+					<jnpf-org-select type="position" v-model="dataForm[items.__vModel__]" :required='items.__config__.required'></jnpf-org-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="divider"
 					v-if="items.__config__.jnpfKey == 'divider'">
@@ -123,17 +123,17 @@
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="address"
 					v-if="items.__config__.jnpfKey == 'address'">
-					<jnpf-city-select v-model="dataForm[items.__vModel__]" placeholder="请选择省市区" :level="2">
+					<jnpf-city-select v-model="dataForm[items.__vModel__]" placeholder="请选择省市区" :level="2" :required='items.__config__.required'>
 					</jnpf-city-select>
 				</u-form-item>
 				<u-form-item :label="items.__config__.label" prop="treeSelect"
 					v-if="items.__config__.jnpfKey == 'treeSelect'">
 					<jnpf-tree-select v-model="dataForm[items.__vModel__]" placeholder="请选择树形选择"
-						:options="items.__config__.options">
+						:options="items.__config__.options" :required='items.__config__.required'>
 					</jnpf-tree-select>
 				</u-form-item>
 				<u-form-item prop="groupTitle" v-if="items.__config__.jnpfKey == 'groupTitle'">
-					<jnpf-group :content="items.content" :content-position="items['content-position']"></jnpf-group>
+					<jnpf-group :content="items.content" :content-position="items['content-position']" :required='items.__config__.required'></jnpf-group>
 				</u-form-item>
 				<!-- 卡片 -->
 				<view class="jnpf-card" v-if="items.__config__.jnpfKey == 'card'">
@@ -170,73 +170,65 @@
 							v-if="card.__config__.jnpfKey == 'currPosition'">
 							<u-input v-model="dataForm[card.__vModel__]" placeholder="系统自动生成" disabled></u-input>
 						</u-form-item>
-
-
+						
 						<u-form-item :label="card.__config__.label" v-if="card.__config__.jnpfKey == 'comInput'">
-							<u-input placeholder="请输入" v-model="dataForm[card.__vModel__]"></u-input>
+							<u-input placeholder="请输入" v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></u-input>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" v-if="card.__config__.jnpfKey == 'textarea'">
-							<u-input placeholder="请输入" v-model="dataForm[card.__vModel__]" type="textarea"></u-input>
+							<u-input placeholder="请输入" v-model="dataForm[card.__vModel__]" type="textarea" :required='card.__config__.required'></u-input>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="checkbox"
 							v-if="card.__config__.jnpfKey == 'checkbox'">
 							<jnpf-select v-model="dataForm[card.__vModel__]" placeholder="请选择下拉框组"
-								:options="card.__slot__.options" :props="card.__config__.props" multiple>
+								:options="card.__config__.options" :props="card.__config__.props" multiple :required='card.__config__.required'>
 							</jnpf-select>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="numInput"
 							v-if="card.__config__.jnpfKey == 'numInput'">
 							<u-number-box v-model="dataForm[card.__vModel__]" :min="card.min" :max="card.max"
-								:step="card.step" :input-width="120" :positive-integer="false" :input-height="60">
+								:step="card.step" :input-width="120" :positive-integer="false" :input-height="60" :required='card.__config__.required'>
 							</u-number-box>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="select"
-							v-if="card.__config__.jnpfKey == 'select'">
+						<u-form-item :label="card.__config__.label" prop="select" v-if="card.__config__.jnpfKey == 'select'">
 							<jnpf-select v-model="dataForm[card.__vModel__]" placeholder="请选择下拉框组"
-								:options="card.__slot__.options" :props="card.__config__.props">
+								:options="card.__config__.options" :props="card.__config__.props" :required='card.__config__.required'>
 							</jnpf-select>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="radio"
-							v-if="card.__config__.jnpfKey == 'radio'">
+						<u-form-item :label="card.__config__.label" prop="radio" v-if="card.__config__.jnpfKey == 'radio'">
 							<u-radio-group v-model="dataForm[card.__vModel__]">
-								<u-radio v-for="(radioCard, radioIndex) in card.__slot__.options" :key="radioIndex"
-									:name="radioCard[card.__config__.props.value]">
-									{{ radioCard[card.__config__.props.label] }}
+								<u-radio v-for="(radios, radioIndex) in card.__config__.options" :key="radioIndex"
+									:name="radios[card.__config__.props.value]" :required='card.__config__.required'>
+									{{ radios[card.__config__.props.label] }}
 								</u-radio>
 							</u-radio-group>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="switch"
-							v-if="card.__config__.jnpfKey == 'switch'">
-							<jnpf-switch v-model="dataForm[card.__vModel__]"></jnpf-switch>
+						<u-form-item :label="card.__config__.label" prop="switch" v-if="card.__config__.jnpfKey == 'switch'">
+							<jnpf-switch v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-switch>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="cascader"
 							v-if="card.__config__.jnpfKey == 'cascader'">
 							<jnpf-cascader v-model="dataForm[card.__vModel__]" placeholder="请选择级联选择"
-								:options="card.options">
+								:options="card.__config__.options" :required='card.__config__.required'>
 							</jnpf-cascader>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="time"
-							v-if="card.__config__.jnpfKey == 'time'">
-							<jnpf-date-time type="time" v-model="dataForm[card.__vModel__]"></jnpf-date-time>
+						<u-form-item :label="card.__config__.label" prop="time" v-if="card.__config__.jnpfKey == 'time'">
+							<jnpf-date-time type="time" v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-date-time>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="date"
-							v-if="card.__config__.jnpfKey == 'date'">
-							<jnpf-date-time type="date" v-model="dataForm[card.__vModel__]"></jnpf-date-time>
+						<u-form-item :label="card.__config__.label" prop="date" v-if="card.__config__.jnpfKey == 'date'">
+							<jnpf-date-time type="date" v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-date-time>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="uploadImg"
 							v-if="card.__config__.jnpfKey == 'uploadImg'">
-							<jnpf-upload v-model="dataForm[card.__vModel__]"></jnpf-upload>
+							<jnpf-upload v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-upload>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="rate"
-							v-if="card.__config__.jnpfKey == 'rate'">
+						<u-form-item :label="card.__config__.label" prop="rate" v-if="card.__config__.jnpfKey == 'rate'">
 							<u-rate v-model="dataForm[card.__vModel__]" size="40"
-								@change="changeRate($event,card.__vModel__)">
+								@change="changeRate($event,card.__vModel__)" :required='card.__config__.required'>
 							</u-rate>
 						</u-form-item>
-						<u-form-item :label="card.__config__.label" prop="slider"
-							v-if="card.__config__.jnpfKey == 'slider'">
-							<u-slider v-model="dataForm[card.__vModel__]" :step="card.step" :min="card.min"
-								:max="card.max" style="width: 100%;">
+						<u-form-item :label="card.__config__.label" prop="slider" v-if="card.__config__.jnpfKey == 'slider'">
+							<u-slider v-model="dataForm[card.__vModel__]" :step="card.step" :min="card.min" :max="card.max"
+								style="width: 100%;" :required='card.__config__.required'>
 								<view class="">
 									<view class="badge-button">
 										{{dataForm[card.__vModel__]}}
@@ -246,38 +238,37 @@
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="comSelect"
 							v-if="card.__config__.jnpfKey == 'comSelect'">
-							<jnpf-org-select type="organize" v-model="dataForm[card.__vModel__]"></jnpf-org-select>
+							<jnpf-org-select type="organize" v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-org-select>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="depSelect"
 							v-if="card.__config__.jnpfKey == 'depSelect'">
-							<jnpf-org-select type="department" v-model="dataForm[card.__vModel__]"></jnpf-org-select>
+							<jnpf-org-select type="department" v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-org-select>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="userSelect"
 							v-if="card.__config__.jnpfKey == 'userSelect'">
-							<jnpf-org-select v-model="dataForm[card.__vModel__]"></jnpf-org-select>
+							<jnpf-org-select v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-org-select>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="posSelect"
 							v-if="card.__config__.jnpfKey == 'posSelect'">
-							<jnpf-org-select type="position" v-model="dataForm[card.__vModel__]"></jnpf-org-select>
+							<jnpf-org-select type="position" v-model="dataForm[card.__vModel__]" :required='card.__config__.required'></jnpf-org-select>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="divider"
 							v-if="card.__config__.jnpfKey == 'divider'">
-							<u-divider>{{card.__slot__.default}}</u-divider>
+							<u-divider>{{card.__config__.default}}</u-divider>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="address"
 							v-if="card.__config__.jnpfKey == 'address'">
-							<jnpf-city-select v-model="dataForm[card.__vModel__]" placeholder="请选择省市区" :level="2">
+							<jnpf-city-select v-model="dataForm[card.__vModel__]" placeholder="请选择省市区" :level="2" :required='card.__config__.required'>
 							</jnpf-city-select>
 						</u-form-item>
 						<u-form-item :label="card.__config__.label" prop="treeSelect"
 							v-if="card.__config__.jnpfKey == 'treeSelect'">
 							<jnpf-tree-select v-model="dataForm[card.__vModel__]" placeholder="请选择树形选择"
-								:options="card.options">
+								:options="card.__config__.options" :required='card.__config__.required'>
 							</jnpf-tree-select>
 						</u-form-item>
 						<u-form-item prop="groupTitle" v-if="card.__config__.jnpfKey == 'groupTitle'">
-							<jnpf-group :content="card.content" :content-position="card['content-position']">
-							</jnpf-group>
+							<jnpf-group :content="card.content" :content-position="card['content-position']" :required='card.__config__.required'></jnpf-group>
 						</u-form-item>
 					</block>
 				</view>
