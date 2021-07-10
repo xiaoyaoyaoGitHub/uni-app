5e79705fb86a019e522ecc1a0aea13aae8b92721
zengjunfeng <20871209@qq.com>
2021-07-10 10:23:18 +0800

    修复小程序子表分装不兼容

--- components/jnpf/jnpf-formControl/index.vue
+++ components/jnpf/jnpf-formControl/index.vue
@@ -3,8 +3,8 @@
 		<u-form :model="dataForm" :rules="rules" ref="dataForm" :errorType="['toast']" label-position="left"
 			label-width="150" label-align="left">
 			<view v-for="(items,index) in formData.fields" :key='index'>
-				<u-form-item v-if="items.__config__.jnpfKey != 'table' && items.__config__.jnpfKey != 'card'" :label="items.__config__.label" :required='items.__config__.required'
-					:prop="items.__vModel__">
+				<u-form-item v-if="items.__config__.jnpfKey != 'table' && items.__config__.jnpfKey != 'card'"
+					:label="items.__config__.label" :required='items.__config__.required' :prop="items.__vModel__">
 					<u-input v-model="dataForm[items.__vModel__]" placeholder="系统自动生成" disabled
 						v-if="items.__config__.jnpfKey == 'billRule'"></u-input>
 					<u-input v-model="dataForm[items.__vModel__]" placeholder="系统自动生成" disabled
@@ -171,9 +171,71 @@
 				</view>
 
 				<!-- 子表 -->
+				<!-- #ifndef MP -->
 				<view v-if="items.__config__.jnpfKey == 'table'">
 					<jnpfChildrenTable :items='items' :dataForm='dataForm' v-if="flag"></jnpfChildrenTable>
 				</view>
+				<!-- #endif -->
+
+				<!-- #ifdef MP-WEIXIN -->
+				<template v-if="items.__config__.jnpfKey == 'table'">
+					<view class="jnpf-table">
+						<view class="jnpf-table-item" v-for="(item,i) in dataForm[items.__vModel__]" :key="i">
+							<view class="jnpf-table-item-title u-flex u-row-between">
+								<text class="jnpf-table-item-title-num">{{items.__config__.label}}({{i+1}})</text>
+								<view class="jnpf-table-item-title-action" @click="delItem(i,items.__vModel__)"
+									v-if="dataForm[items.__vModel__].length > 1">删除</view>
+							</view>
+							<view v-for="(children,c) in items.__config__.children" :key='c'>
+								<u-form-item :label="children.__config__.label" :prop="children.__vModel__">
+									<u-input placeholder="请输入"
+										v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey === 'comInput'" />
+									<u-number-box v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										:min="children.min" :max="children.max" :step="children.step" :input-width="120"
+										:positive-integer="false" :input-height="60"
+										v-if="children.__config__.jnpfKey == 'numInput'" />
+									<jnpf-select v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										placeholder="请选择下拉框组" :options="children.__slot__.options"
+										:props="children.__config__.props"
+										v-if="children.__config__.jnpfKey == 'select'" />
+									<jnpf-tree-select v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										:placeholder="children.placeholder" :options="children.options"
+										v-if="children.__config__.jnpfKey == 'treeSelect'" />
+									<jnpf-cascader v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										placeholder="请选择级联选择" :options="children.options"
+										v-if="children.__config__.jnpfKey == 'cascader'" />
+									<jnpf-date-time type="date"
+										v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'date'" />
+									<jnpf-date-time type="time"
+										v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'time'" />
+									<jnpf-city-select v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										placeholder="请选择省市区" :level="2"
+										v-if="children.__config__.jnpfKey == 'address'" />
+									<jnpf-org-select type="organize"
+										v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'comSelect'" />
+									<jnpf-org-select type="department"
+										v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'depSelect'" />
+									<jnpf-org-select v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'userSelect'" />
+									<jnpf-org-select type="position"
+										v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'posSelect'" />
+									<jnpf-switch v-model="dataForm[items.__vModel__][i][children.__vModel__]"
+										v-if="children.__config__.jnpfKey == 'switch'" />
+								</u-form-item>
+							</view>
+						</view>
+						<view class="jnpf-table-addBtn" @click="addTable(items)">
+							<u-icon name="plus" color="#2979ff"></u-icon>添加
+						</view>
+					</view>
+				</template>
+				<!-- #endif -->
 			</view>
 		</u-form>
 	</view>
@@ -192,7 +254,7 @@
 					return {}
 				}
 			},
-			
+
 			webType: {
 				type: String,
 				default () {
@@ -203,7 +265,7 @@
 		data() {
 			return {
 				setting: {},
-				dataForm:{},
+				dataForm: {},
 				rules: {},
 				tableVmodel: [],
 				options: [],
@@ -212,14 +274,14 @@
 				},
 				isId: false,
 				featuresId: '',
-				flag:false
+				flag: false
 			}
 		},
 		mounted() {
 			this.$refs.dataForm.setRules(this.rules);
 			this.init();
 		},
-		
+
 		methods: {
 			init() {
 				let fields = this.formData.fields;
@@ -294,7 +356,9 @@
 							}
 							if (vModel) {
 								this.dataForm[vModel] = [];
-								// this.dataForm[vModel].push(item);
+								// #ifdef MP-WEIXIN
+								this.dataForm[vModel].push(item);
+								// #endif
 								this.flag = true
 							}
 						}
@@ -304,7 +368,24 @@
 			},
 
 
-
+			// #ifdef MP-WEIXIN
+			addTable(items) {
+				this.$forceUpdate();
+				let childItem = {};
+				let list = this.dataForm[items.__vModel__];
+				for (var j = 0; j < items.__config__.children.length; j++) {
+					let e = items.__config__.children[j]
+					childItem[e.__vModel__] = '';
+				}
+				this.dataForm[items.__vModel__].push(childItem)
+				this.$forceUpdate();
+			},
+			delItem(i, model) {
+				this.$forceUpdate();
+				this.dataForm[model].splice(i, 1);
+				this.$forceUpdate();
+			},
+			// #endif
 
 
 			submitForm() {
