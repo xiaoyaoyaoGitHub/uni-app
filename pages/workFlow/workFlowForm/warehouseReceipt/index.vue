738ffd2ef5c4df8f2a95c7890641d21815ffd50e
zengjunfeng <20871209@qq.com>
2021-08-07 15:13:51 +0800

    入库子表权限

--- pages/workFlow/workFlowForm/warehouseReceipt/index.vue
+++ pages/workFlow/workFlowForm/warehouseReceipt/index.vue
@@ -50,7 +50,7 @@
 
 			</view>
 
-			<view class="jnpf-table">
+			<view class="jnpf-table" v-if="judgeShow('entryList')">
 				<view class="jnpf-table-item" v-for="(item,i) in dataForm.entryList" :key="i">
 					<view class="jnpf-table-item-title u-flex u-row-between">
 						<text class="jnpf-table-item-title-num">入库明细({{i+1}})</text>
