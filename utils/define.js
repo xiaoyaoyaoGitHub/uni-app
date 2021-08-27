cb4d89a7ae75a4f3056c8b8bbd3958dd7c1e85d3
zengjunfeng <20871209@qq.com>
2021-08-27 15:08:15 +0800

    接口调整

--- utils/define.js
+++ utils/define.js
@@ -1,9 +1,10 @@
 /* process.env.NODE_ENV设置生产环境模式 */
+/*JAVA app.java.jnpfsoft.com  NET dotnet.jnpfsoft.com*/
 const baseURL = process.env.NODE_ENV === "production" ?
-	"https://java.jnpfsoft.com" : "http://192.168.0.18:7004" // 接口前缀
+	"http://192.168.0.18:7004" : "http://192.168.0.18:7004" // 接口前缀
 const define = {
 	baseURL: baseURL,
-	webSocketUrl: process.env.NODE_ENV === "production" ? "wss://java.jnpfsoft.com/api/message/websocket" :
+	webSocketUrl: process.env.NODE_ENV === "production" ? "ws://192.168.0.18:7004/api/message/websocket" :
 		"ws://192.168.0.18:7004/api/message/websocket",
 	comUploadUrl: baseURL + '/api/file/Uploader/',
 	timeout: process.env.NODE_ENV === 'production' ? 60000 : 10000,
