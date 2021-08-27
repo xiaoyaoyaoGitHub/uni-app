14261dc287002803c187b98fe0277b6e0ce3b161
zengjunfeng <20871209@qq.com>
2021-08-27 15:37:12 +0800

    修改电话号码验证

--- pages/workFlow/workFlowForm/contractApproval/index.vue
+++ pages/workFlow/workFlowForm/contractApproval/index.vue
@@ -173,6 +173,18 @@
 						message: '合同名称不能为空',
 						trigger: 'change'
 					}],
+					firstPartyContact:[{
+						pattern: /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/,
+						message: '手机号码不正确',
+						type: 'string',
+						trigger: ['change', 'blur'],
+					}],
+					secondPartyContact:[{
+						pattern: /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/,
+						message: '手机号码不正确',
+						type: 'string',
+						trigger: ['change', 'blur'],
+					}],
 				}
 			}
 		},
