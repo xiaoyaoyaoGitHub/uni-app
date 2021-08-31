06657bb3be2dfce8021560cf9e2f24270ba61590
zengjunfeng <20871209@qq.com>
2021-08-31 09:25:30 +0800

    Java打包配置

--- utils/define.js
+++ utils/define.js
@@ -1,10 +1,10 @@
 /* process.env.NODE_ENV设置生产环境模式 */
 /*JAVA app.java.jnpfsoft.com  NET dotnet.jnpfsoft.com*/
 const baseURL = process.env.NODE_ENV === "production" ?
-	"http://192.168.0.18:7004" : "http://192.168.0.18:7004" // 接口前缀
+	"https://app.java.jnpfsoft.com" : "http://192.168.0.18:7004" // 接口前缀
 const define = {
 	baseURL: baseURL,
-	webSocketUrl: process.env.NODE_ENV === "production" ? "ws://192.168.0.18:7004/api/message/websocket" :
+	webSocketUrl: process.env.NODE_ENV === "production" ? "wss://app.java.jnpfsoft.com/websocket" :
 		"ws://192.168.0.18:7004/api/message/websocket",
 	comUploadUrl: baseURL + '/api/file/Uploader/',
 	timeout: process.env.NODE_ENV === 'production' ? 60000 : 10000,
