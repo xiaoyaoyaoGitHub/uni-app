43cc16bc80aa864de909f4bcf943d70338954b2d
zhm <zhuhuimin@yinmaisoft.com>
2021-09-07 15:31:21 +0800

    企业组织优化

--- pages/my/business/index.vue
+++ pages/my/business/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="business-v">
 		<u-cell-group>
-			<u-cell-item title="公司" :value="userInfo.organizeName" :arrow="false"></u-cell-item>
-			<u-cell-item title="部门" :value="userInfo.departmentName" :arrow="false"></u-cell-item>
+			<u-cell-item title="组织" :value="userInfo.organizeName" :arrow="false"></u-cell-item>
+			<!-- <u-cell-item title="部门" :value="userInfo.departmentName" :arrow="false"></u-cell-item> -->
 			<u-cell-item title="岗位" :value="positionIds" :arrow="false"></u-cell-item>
 			<u-cell-item title="角色" :value="userInfo.roleName" :arrow="false"></u-cell-item>
 		</u-cell-group>
